2f4d12bb144c633b75923cce151c0ed5fee7c20d
eloymendoza <146760444+eloymendoza@users.noreply.github.com>
2026-06-30 15:15:28 -0600

    feat(database): migraciones para puestos propuestos en requisiciones

---
+++ database/migrations/2026_06_30_173206_create_propuesta_puestos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('propuesta_puestos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('detalle_requisicion_id')->constrained('detalle_requisiciones')->onDelete('cascade');
+            $table->string('nombre_puesto', 255);
+            $table->unsignedBigInteger('reporta_a_puesto_id')->nullable();
+            $table->tinyInteger('tipo');
+            $table->timestamps(7);
+            $table->softDeletes('deleted_at', 7);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('propuesta_puestos');
+    }
+};
